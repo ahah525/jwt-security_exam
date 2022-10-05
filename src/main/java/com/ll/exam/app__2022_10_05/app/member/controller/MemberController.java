992166590b44af12f1fde1c74510bc7622754684
ahah525 <ahah525@naver.com>
2022-10-05 22:53:21 +0900

    feat : 존재하지 않는 회원이거나 잘못된 비밀번호 입력시 400 응답 처리

--- src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/controller/MemberController.java
@@ -1,9 +1,13 @@
 package com.ll.exam.app__2022_10_05.app.member.controller;
 
+import com.ll.exam.app__2022_10_05.app.member.entity.Member;
+import com.ll.exam.app__2022_10_05.app.member.service.MemberService;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +15,25 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/member")
+@RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
+    private final PasswordEncoder passwordEncoder;
+
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
         if(loginDto.isNotValid()) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+
+        Member member = memberService.findByUsername(loginDto.getUsername()).orElse(null);
+        // 1. 존재하지 않는 회원
+        if (member == null) {
+            return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
+        }
+        // 2. 올바르지 않은 비밀번호
+        // matches(비밀번호 원문, 암호화된 비밀번호)
+        if(passwordEncoder.matches(loginDto.getPassword(), member.getPassword()) == false) {
             return new ResponseEntity<>(null, null, HttpStatus.BAD_REQUEST);
         }
 
