0ceb4c8a4e33e091e88c435e9c0c7605502859b3
ahah525 <ahah525@naver.com>
2022-10-05 21:02:46 +0900

    feat : Member entity, service, repository 추가 및 테스트 환경에서 회원 2명 생성

---
+++ src/main/java/com/ll/exam/app__2022_10_05/app/base/entity/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.ll.exam.app__2022_10_05.app.base.entity;
+
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+@Getter
+@SuperBuilder
+@MappedSuperclass
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@ToString
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime modifyDate;
+
+    @Transient
+    @Builder.Default
+    private Map<String, Object> extra = new LinkedHashMap<>();
+
+    public BaseEntity(long id) {
+        this.id = id;
+    }
+}
