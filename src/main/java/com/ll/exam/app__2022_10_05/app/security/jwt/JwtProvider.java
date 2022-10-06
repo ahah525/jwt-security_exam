46dbd94c6851b3ae11afdcd587c34a9d066163d1
ahah525 <ahah525@naver.com>
2022-10-06 21:40:24 +0900

    feat : accessToken 생성을 위한 jwt 기초 작업

---
+++ src/main/java/com/ll/exam/app__2022_10_05/app/security/jwt/JwtProvider.java
@@ -0,0 +1,57 @@
+package com.ll.exam.app__2022_10_05.app.security.jwt;
+
+import com.ll.exam.app__2022_10_05.util.Util;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.util.Date;
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
+public class JwtProvider {
+    private final SecretKey jwtSecretKey;
+
+    private SecretKey getSecretKey() {
+        return jwtSecretKey;
+    }
+
+
+    public String generateAccessToken(Map<String, Object> claims, int seconds) {
+        long now = new Date().getTime();
+        Date accessTokenExpiresIn = new Date(now + 1000L * seconds);
+
+        return Jwts.builder()
+                .claim("body", Util.json.toStr(claims))         // payload에 들어갈 정보 조각
+                .setExpiration(accessTokenExpiresIn)                // accessToken 만료 시간
+                .signWith(getSecretKey(), SignatureAlgorithm.HS512) // secretKey로 암호화
+                .compact();
+    }
+
+    public boolean verify(String accessToken) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(getSecretKey())
+                    .build()
+                    .parseClaimsJws(accessToken);
+        } catch (Exception e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public Map<String, Object> getClaims(String accessToken) {
+        String body = Jwts.parserBuilder()
+                .setSigningKey(getSecretKey())
+                .build()
+                .parseClaimsJws(accessToken)
+                .getBody()
+                .get("body", String.class);
+
+        return Util.json.toMap(body);
+    }
+}
--- src/main/java/com/ll/exam/app__2022_10_05/util/Util.java
+++ src/main/java/com/ll/exam/app__2022_10_05/util/Util.java
@@ -1,5 +1,8 @@
 package com.ll.exam.app__2022_10_05.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ll.exam.app__2022_10_05.app.AppConfig;
 import com.ll.exam.app__2022_10_05.app.base.dto.RsData;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -9,6 +12,33 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class Util {
+    // jwt 관련
+    private static ObjectMapper getObjectMapper() {
+        return (ObjectMapper) AppConfig.getContext().getBean("objectMapper");
+    }
+
+    public static class json {
+
+        // map(json) -> String 변환
+        public static Object toStr(Map<String, Object> map) {
+            try {
+                return getObjectMapper().writeValueAsString(map);
+            } catch (JsonProcessingException e) {
+                return null;
+            }
+        }
+
+        // String -> map(json) 변환
+        public static Map<String, Object> toMap(String jsonStr) {
+            try {
+                return getObjectMapper().readValue(jsonStr, LinkedHashMap.class);
+            } catch (JsonProcessingException e) {
+                return null;
+            }
+        }
+    }
+    // 추가 끝
+
     // 인자 값들을 map 형태로 반환
     public static<K, V> Map<K, V> mapOf(Object... args) {
         Map<K, V> map = new LinkedHashMap<>();
