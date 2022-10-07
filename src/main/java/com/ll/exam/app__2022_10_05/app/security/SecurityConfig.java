2a59fe68576a045023030500bf62b905d31f3163
ahah525 <ahah525@naver.com>
2022-10-07 15:00:42 +0900

    feat : 시큐리티 커스텀 필터(AuthorizationFilter)를 추가해 로그인,회원가입 외 모든 요청 로그인 필수처리

--- src/main/java/com/ll/exam/app__2022_10_05/app/security/SecurityConfig.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/security/SecurityConfig.java
@@ -1,11 +1,13 @@
 package com.ll.exam.app__2022_10_05.app.security;
 
+import com.ll.exam.app__2022_10_05.app.security.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
@@ -13,16 +15,29 @@ import static org.springframework.security.config.http.SessionCreationPolicy.STA
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
         http
+                .authorizeRequests(
+                        // 로그인, 회원가입 외 모든 요청은 로그인 필수
+                        authorizeRequests -> authorizeRequests
+                                .antMatchers("/member/login", "/member/join")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated()
+                )
                 .cors().disable() // 타 도메인에서 API 호출 가능
                 .csrf().disable() // CSRF 토큰 끄기
                 .httpBasic().disable() // httpBaic 로그인 방식 끄기
                 .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
-                ); // 세션 사용안함
+                ).addFilterBefore(
+                        jwtAuthorizationFilter,
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
     }
