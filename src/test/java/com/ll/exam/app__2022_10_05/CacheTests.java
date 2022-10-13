afc640389f0b88cde797ea062a0907a04ce36780
ahah525 <ahah525@naver.com>
2022-10-13 22:31:48 +0900

    test : 캐시 데이터 삭제 테스트 추가

--- src/test/java/com/ll/exam/app__2022_10_05/CacheTests.java
+++ src/test/java/com/ll/exam/app__2022_10_05/CacheTests.java
@@ -8,8 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 
 @SpringBootTest
 @Transactional
@@ -21,14 +19,27 @@ class CacheTests {
     @Test
     @DisplayName("캐시 사용")
     void t1() throws Exception {
+        // O
         int rs = memberService.getCachedInt();
-
-        assertThat(rs).isGreaterThan(0);
         System.out.println(rs);
 
         rs = memberService.getCachedInt();
+        System.out.println(rs);
+    }
 
-        assertThat(rs).isGreaterThan(0);
+    @Test
+    @DisplayName("캐시 삭제")
+    void t2() throws Exception {
+        // OXO
+        int rs = memberService.getCachedInt();
+        System.out.println(rs);
+
+        rs = memberService.getCachedInt();
+        System.out.println(rs);
+
+        memberService.deleteCacheKey1();
+
+        rs = memberService.getCachedInt();
         System.out.println(rs);
     }
 }
